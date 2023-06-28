2d84360db3b7a19c2bb82df96d43c596b3fefc7a
team7 <hnhyerim@gmail.com>
2023-06-28 17:31:15 +0900

    Feat: auth interface 생성

---
+++ src/interfaces/auth.ts
@@ -0,0 +1,14 @@
+export interface IAuth {
+  email: string;
+  password: string;
+}
+
+export interface IAuthValid {
+  isEmail: boolean;
+  isPassword: boolean;
+}
+
+export interface IAuthValidError {
+  emailError: string;
+  passwordError: string;
+}
