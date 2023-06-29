70e120f9a8b2337406560e9ebf25434ca76994f6
team7 <hnhyerim@gmail.com>
2023-06-29 19:57:29 +0900

    Style: router 원상복구

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Navigate, createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import NotFound from '../pages/NotFound';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import Home from '../pages/Home';
 import Todo from 'pages/Todo';
-import { getLocalStorage } from 'utils/storage';
 
-const token = getLocalStorage('accessToken');
 const router = createBrowserRouter([
   {
     path: '/',
@@ -18,14 +16,14 @@ const router = createBrowserRouter([
       {
         index: true,
         path: '/',
-        element: token ? <Navigate to="/todo" /> : <Home />,
+        element: <Home />,
       },
-      { path: '/signin', element: token ? <Navigate to="/todo" /> : <Login /> },
+      { path: '/signin', element: <Login /> },
       {
         path: '/signup',
-        element: token ? <Navigate to="/todo" /> : <Signup />,
+        element: <Signup />,
       },
-      { path: '/todo', element: !token ? <Navigate to="/signin" /> : <Todo /> },
+      { path: '/todo', element: <Todo /> },
     ],
   },
 ]);
