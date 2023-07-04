fbc56e6ab450cf491c89d453047787746a017f7d
team7 <hnhyerim@gmail.com>
2023-07-04 18:57:10 +0900

    Refactor: 각 페이지에 맞는 api 함수(함수는 한번에 한가지만)

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,30 @@
+import { SIGNUP } from 'apis/auth';
 import AuthContainer from 'components/auth/AuthContainer';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-  return <AuthContainer title="회원가입" dataTestid="signup-button" />;
+  const navigate = useNavigate();
+
+  const signup = async (email: string, password: string) => {
+    try {
+      await SIGNUP({
+        email,
+        password,
+      });
+      alert('회원가입이 성공하였습니다!\n로그인을 시도해주세요.');
+      navigate('/signin');
+    } catch (error: any) {
+      alert(error.response.data.message);
+    }
+  };
+
+  return (
+    <AuthContainer
+      title="회원가입"
+      dataTestid="signup-button"
+      onSubmit={signup}
+    />
+  );
 };
 
 export default Signup;
